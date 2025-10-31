6bc200618285e322ab3deadf295e4e0c7499830b
Milo Germanus <milogermanus@gmail.com>
2025-10-31 13:28:37 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,9 @@
 # Startup script for the Developing TV dashboard
 sudo apt-get update
 sudo apt-get -y upgrade
+sudo apt-get install npm
 cd ~/developing-dashboard
+git pull
 sudo npm update
 xdg-open https://nederland.fm/radio/joe
 xdg-open http://localhost:5173/
