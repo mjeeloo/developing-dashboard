4df05bcf35211b2bab949573a02f269f3f3cc6fe
Milo Germanus <milogermanus@gmail.com>
2025-10-31 14:12:46 +0100

    Update startup.sh
    Comment out auto update

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Startup script for the Developing TV dashboard
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y install nodejs npm
+# sudo apt-get update
+# sudo apt-get -y upgrade
+# sudo apt-get -y install nodejs npm
 cd ~/developing-dashboard
 git pull
-sudo npm update
+# sudo npm update
 open https://nederland.fm/radio/joe &
 npm run dev
 open http://localhost:5173/ &
