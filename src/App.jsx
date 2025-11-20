793f222071b515933e93d37c1fb83b86ab2e3a7f
Milo Germanus <milogermanus@gmail.com>
2025-11-20 11:57:25 +0100

    Radio card

--- src/App.jsx
+++ src/App.jsx
@@ -911,7 +911,7 @@ function App() {
             <article className="surface-card radio-card" aria-label="TuneIn music embed">
               <div className="radio-embed-wrapper">
                 <iframe
-                  src="https://nederland.fm/"
+                  src="https://nederland.fm/radio/joe"
                   title="nederland.fm music stations"
                   allow="autoplay; encrypted-media"
                   loading="lazy"
